ae2468da128346e7de1a6cc2d76df7913b5d127e
Saza-ku <the.latticeheart@gmail.com>
2023-09-19 18:34:54 +0900

    add index to comments

--- sqlall.sh
+++ sqlall.sh
@@ -7,11 +7,7 @@ MYSQL_DBNAME=isuconp
 cd `dirname $0`
 
 rsync -a ./scripts/exec.sql isucon@isucon1:~/scripts/exec.sql &
-rsync -a ./scripts/exec.sql isucon@isucon2:~/scripts/exec.sql &
-rsync -a ./scripts/exec.sql isucon@isucon3:~/scripts/exec.sql &
 wait
 
 ssh isucon@isucon1 "sudo mysql $MYSQL_DBNAME < ~/scripts/exec.sql" &
-ssh isucon@isucon2 "sudo mysql $MYSQL_DBNAME < ~/scripts/exec.sql" &
-ssh isucon@isucon3 "sudo mysql $MYSQL_DBNAME < ~/scripts/exec.sql" &
 wait
